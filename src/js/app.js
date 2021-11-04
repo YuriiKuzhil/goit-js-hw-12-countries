1481cbcbb4c244c2a410fda24fd86dbf8cb6e9ea
Yurii <yuriikuzhil@gmail.com>
2021-11-04 22:52:56 +0200

    updated general code

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,17 @@
 import fetchContries from './fetchCountries.js';
 import countryCardTpl from '../templates/country-card.hbs';
 import countriesListTpl from '../templates/countries-list.hbs';
-import '@pnotify/core/dist/BrightTheme.css';
-import { info, error, Stack } from '@pnotify/core';
+import { info, error } from '@pnotify/core';
+import showAlert from './alert-message';
 
+const MATCH_RESULTS = 'Too many matches found. Please enter a more specific query!';
+const NOT_FOUND = 'This country has not been found. Check country name';
 const debounce = require('lodash.debounce');
 const refs = {
   contentWrapper: document.querySelector('.content-wrapper'),
   input: document.querySelector('.input'),
 };
-const stack = new Stack({
-  dir1: 'down',
-  dir2: 'right',
-  firstpos1: 90,
-  firstpos2: 90,
-});
+
 function renderCountry(countries) {
   refs.contentWrapper.innerHTML = '';
   if (countries.length == 1) {
@@ -22,23 +19,15 @@ function renderCountry(countries) {
   } else if (countries.length > 1 && countries.length <= 10) {
     return refs.contentWrapper.insertAdjacentHTML('beforeend', countriesListTpl(countries));
   } else if (countries.length > 10) {
-    onRenderError(error, 'Too many matches found. Please enter a more specific query!');
+    showAlert(info, MATCH_RESULTS);
     return;
   } else {
-    onRenderError(info, 'No matches found!');
+    showAlert(error, NOT_FOUND);
   }
 }
 
 const onSearchCountries = event => {
   let inputValue = event.target.value;
-  fetchContries(inputValue)
-    .then(renderCountry)
-    .catch(error => {
-      alert('ERROR! Please, try later');
-    });
+  fetchContries(inputValue).then(renderCountry);
 };
 refs.input.addEventListener('input', debounce(onSearchCountries, 500));
-
-function onRenderError(type, text) {
-  type({ text: `${text}`, delay: 2000, stack: stack });
-}
